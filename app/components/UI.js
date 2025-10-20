4466ae7313d199b96e71ceca36740cc1e3c24d35
Azz <arisachi7201@gmail.com>
2025-10-20 12:57:44 +0530

    after update of UI design on oct 20 2025

--- app/components/UI.js
+++ app/components/UI.js
@@ -6,12 +6,12 @@ import { FaUser } from 'react-icons/fa'
 
 function UI() {
   return (
-    <div className="bg-transparent fixed top-110 w-full  flex  justify-center items-center">
-   <div className="ui   flex   text-white bg-neutral-700  rounded-2xl px-2 py-2 shadow-lg shadow-neutral-900/50 backdrop-blur-md">
+    <div className="bg-transparent fixed top-100 md:top-110 left-0  w-full  flex   justify-center items-center">
+   <div className="ui flex  text-white bg-neutral-700 opacity-90 rounded-2xl px-2 py-2 shadow-lg shadow-neutral-900/50 backdrop-blur-md ">
     <div className="flex gap-10 mx-2 my-1 group ">
     <Link href={'#home'}><FaHouse size={20}/></Link>
-    <Link href={'#projects'}><FaBarsProgress size={20}/></Link>
     <Link href={'#about'}><FaCircleInfo size={20}/></Link>
+    <Link href={'#projects'}><FaBarsProgress size={20}/></Link>
     <Link href={'#contact'}><FaUser size={20}/></Link>
 
    
